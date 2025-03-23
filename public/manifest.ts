0a9e97bc55252af0236d6b10bef564362543458d
haocloo <81066169+haocloo@users.noreply.github.com>
2025-03-23 22:41:47 +0800

    feat: pwa

---
+++ public/manifest.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'AllergyGuard',
+    short_name: 'AllergyGuard',
+    description:
+      "AllergyGuard is a mobile web-based app that helps manage children's food allergies. It centralizes allergy profiles, detects allergens in real time, plans safe meals, and supports emergency care using AI.",
+    start_url: 'https://allergy-guard.vercel.app',
+    display: 'standalone',
+    theme_color: '#8936FF',
+    background_color: '#2EC6FE',
+    orientation: 'any',
+    dir: 'auto',
+    lang: 'en-US',
+    scope: 'https://allergy-guard.vercel.app',
+    id: 'https://allergy-guard.vercel.app',
+    icons: [
+      {
+        purpose: 'maskable',
+        sizes: '512x512',
+        src: 'icon512_maskable.png',
+        type: 'image/png',
+      },
+      { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+    ],
+  };
+}
